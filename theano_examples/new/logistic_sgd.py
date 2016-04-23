651549099c556bfa2c25fc6cde67977c2bdb0df5
WangZ <WangZhiCS@gmail.com>
2016-04-23 12:29:55 +0800

    finish softmax  with sgd

--- theano_examples/new/logistic_sgd.py
+++ theano_examples/new/logistic_sgd.py
@@ -2,54 +2,14 @@ import numpy
 import theano
 import theano.tensor as T
 from theano_examples.new.model import SoftMax
-from theano_examples.new.train import TrainModel
+from theano_examples.new.train import BATCH_SIZE, load_data, sgd_train
 
-
-class LogisticModel(TrainModel):
-    def __init__(self, train_set, valid_set):
-        train_set_x, train_set_y = train_set
-        valid_set_x, valid_set_y = valid_set
-        soft_max = SoftMax(28*28, 10)
-        g_w = T.grad(cost=soft_max.cost, wrt=soft_max.w)
-        g_b = T.grad(cost=soft_max.cost, wrt=soft_max.b)
-        learning_rate = 0.15
-        updates = [(soft_max.w, soft_max.w - learning_rate * g_w),
-                   (soft_max.b, soft_max.b - learning_rate * g_b)]
-        index = T.lscalar()
-        train_set_size = train_set.get_value(borrow=True).shape[0]
-        batch_size = train_set_size // TrainModel.BATCH_COUNT
-        self.train_model = theano.function(
-            inputs=[index],
-            outputs=soft_max.cost,
-            updates=updates,
-            givens={
-                soft_max.x: train_set_x[index * batch_size: (index + 1) * batch_size],
-                soft_max.y: train_set_y[index * batch_size: (index + 1) * batch_size]
-            }
-        )
-        self.validate_model = theano.function(
-            inputs=[index],
-            outputs=soft_max.errors,
-            givens={
-                soft_max.x: valid_set_x[index * batch_size: (index + 1) * batch_size],
-                soft_max.y: valid_set_y[index * batch_size: (index + 1) * batch_size]
-            }
-        )
-
-        def train_batch(self, batch_index):
-            return self.train_model
-
-        def valid_error(self):
-            validation_losses = [self.validate_model(i) for i in range(n_valid_batches)]
-            return numpy.mean(validation_losses)
-
-        def finish_once(self):
-            pass
-
-        def finish(self):
-            pass
 
 if __name__ == '__main__':
+    # load data
+    train_set, valid_set, test_set = load_data()
+    train_set_x, train_set_y = train_set
+    valid_set_x, valid_set_y = valid_set
     soft_max = SoftMax(28*28, 10)
     g_w = T.grad(cost=soft_max.cost, wrt=soft_max.w)
     g_b = T.grad(cost=soft_max.cost, wrt=soft_max.b)
@@ -57,3 +17,31 @@ if __name__ == '__main__':
     updates = [(soft_max.w, soft_max.w - learning_rate * g_w),
                (soft_max.b, soft_max.b - learning_rate * g_b)]
     index = T.lscalar()
+    train_model = theano.function(
+        inputs=[index],
+        outputs=soft_max.cost,
+        updates=updates,
+        givens={
+            soft_max.x: train_set_x[index * BATCH_SIZE: (index + 1) * BATCH_SIZE],
+            soft_max.y: train_set_y[index * BATCH_SIZE: (index + 1) * BATCH_SIZE]
+        }
+    )
+    validate_model = theano.function(
+        inputs=[index],
+        outputs=soft_max.error,
+        givens={
+            soft_max.x: valid_set_x[index * BATCH_SIZE: (index + 1) * BATCH_SIZE],
+            soft_max.y: valid_set_y[index * BATCH_SIZE: (index + 1) * BATCH_SIZE]
+        }
+    )
+    # remember the best
+    best_w = soft_max.w.get_value(borrow=False)
+    best_b = soft_max.b.get_value(borrow=False)
+
+    def finish_once(improve):
+        if improve:
+            best_w = soft_max.w.get_value(borrow=False)
+            best_b = soft_max.b.get_value(borrow=False)
+    sgd_train(train_set, valid_set, train_model, validate_model, finish_once)
+    soft_max.w.set_value(best_w)
+    soft_max.b.set_value(best_b)
--- theano_examples/new/train.py
+++ theano_examples/new/train.py
@@ -13,50 +13,48 @@ def shared_dataset(data_xy, borrow=True):
 
 
 def load_data():
-    with gzip.open('../mnist.pkl.gz', 'rb') as f:
+    with gzip.open('../../mnist.pkl.gz', 'rb') as f:
             train_set, valid_set, test_set = pickle.load(f, encoding='latin1')
             return shared_dataset(train_set), shared_dataset(valid_set), shared_dataset(test_set)
 
+BATCH_SIZE = 600
 
-class TrainModel:
 
-    BATCH_COUNT = 100
+def sgd_train(train_set, valid_set, train_model, valid_model, finish_once):
+    train_set_x, train_set_y = train_set
+    valid_set_x, valid_set_y = valid_set
+    train_set_size = train_set_x.get_value(borrow=True).shape[0]
+    valid_set_size = valid_set_x.get_value(borrow=True).shape[0]
+    train_set_batch = train_set_size // BATCH_SIZE
+    valid_set_batch = valid_set_size // BATCH_SIZE
+    # sgd train
+    running = True
+    best_valid_error_rate = 1.0
+    # early stop
+    not_improve = 0
+    i = 0
+    while running:
+        _train_one(train_model, train_set_batch)
+        valid_error_rate = _valid_error(valid_model, valid_set_batch)
+        if valid_error_rate < best_valid_error_rate:
+            best_valid_error_rate = valid_error_rate
+            finish_once(True)
+            not_improve = 0
+        else:
+            finish_once(False)
+            not_improve += 1
+        if not_improve > 5:
+            running = False
+        # debug info
+        print("train step %d , valid_error_rate %f%%" % (i, valid_error_rate*100))
+        i += 1
 
-    def __init__(self, model_size):
-        self.batch_count = model_size // 100
 
-    def train_batch(self, batch_index):
-        pass
+def _train_one(train_model, train_count):
+        for i in range(train_count):
+            train_model(i)
 
-    def valid_error(self):
-        pass
 
-    def finish_once(self):
-        pass
-
-    def finish(self):
-        pass
-
-    def train_one(self):
-        for i in range(TrainModel.BATCH_COUNT):
-            self.train_batch(i)
-
-    def get_batch_count(self, set_size):
-        return set_size // TrainModel.BATCH_COUNT
-
-    def run(self):
-        running = True
-        # early stop
-        not_improve = 0
-        i = 0
-        while running:
-            self.train_one()
-            valid_error_rate = self.valid_error()
-            if valid_error_rate < best_valid_error_rate:
-                best_valid_error_rate = valid_error_rate
-                self.save_params()
-                not_improve = 0
-            else:
-                not_improve += 1
-            if not_improve > 5:
-                running = False
+def _valid_error(valid_model, batch_count):
+    validation_losses = [valid_model(i) for i in range(batch_count)]
+    return numpy.mean(validation_losses)
